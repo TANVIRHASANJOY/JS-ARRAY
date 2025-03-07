434905f70db23c94dc2f8169c0636435e87b8d6d
tanvir hasan joy <tanvirhasanjoy18@gmail.com>
2025-03-07 17:09:14 +0600

    first commit

---
+++ array.js
@@ -0,0 +1,3 @@
+const numbers = [10,20,30];
+const friends=['tanvir','hasan','joy']
+console.log( numbers )
---
+++ length.js
@@ -0,0 +1,5 @@
+let numbers=[1,2,3,4,5,6,7,8,99]
+console.log(numbers.length)
+numbers[3]='change'
+console.log(numbers[3])
+console.log(numbers)
